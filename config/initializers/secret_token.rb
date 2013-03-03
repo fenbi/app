d928d13623c1f0e33757c75f452145f169b7e6b2
liluo <i@liluo.org>
2013-03-03 13:36:06 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '6af8d1ff1b27783b3be63586eb4d725544310c0c63a04ee1140182189bc95430631ac5da7f40681042513759ef2a12cd46603f20a01fd998c97aca80f7dd883f'
